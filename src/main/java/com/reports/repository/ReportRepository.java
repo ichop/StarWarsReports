c0fd3685ba41eb0efaee787439751289b956ead9
Ivan <vanyachop@gmail.com>
2020-07-15 23:15:43 +0200

    Release 1.0.0

--- src/main/java/com/reports/repository/ReportRepository.java
+++ src/main/java/com/reports/repository/ReportRepository.java
@@ -2,6 +2,8 @@ package com.reports.repository;
 
 import com.reports.model.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReportRepository extends JpaRepository<Report, Integer> {
 }
